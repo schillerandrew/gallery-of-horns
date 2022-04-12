b3fece0976ea99e53805e126be4239cf5a2387e4
schillerandrew <hyogoetophile@gmail.com>
2022-04-11 17:11:51 -0700

    removed unneeded files, updated React version, added components: Footer Header HornedBeast and Main

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HornedBeast from './HornedBeast';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <>
+      <HornedBeast />
+      <HornedBeast />
+      </>
+    )
+  }
+}
+
+export default Main;
